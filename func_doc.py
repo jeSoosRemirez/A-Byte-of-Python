9454a6dba9f66739064f18d666e018d55e4fe147
jeSoosRemirez <dkhomych0091@gmail.com>
2019-01-03 12:02:28 +0200

    Create func_doc.py

---
+++ func_doc.py
@@ -0,0 +1,15 @@
+def printMax(x, y):
+    '''Bla bla bla
+
+    Blah blah blah corectly...'''
+    x = int(x)
+    y = int(y)
+
+    if x > y:
+        print(x, 'the most')
+    elif x < y:
+        print(y, 'the most')
+
+
+printMax(3, 5)
+print(printMax.__doc__)
