f81cf004e28bd277206a82ab41babfcf15d22c07
jeSoosRemirez <dkhomych0091@gmail.com>
2019-01-04 10:33:15 +0200

    Create from_math.py

---
+++ from_math.py
@@ -0,0 +1,21 @@
+from math import *
+n = input('Pls, type numbers: ')
+n = int(n)
+p = [2, 3]
+count = 2
+a = 5
+while count < n:
+    b = 0
+    for i in range(2, a):
+        if i <= sqrt(a):
+            if a % 1 == 0:
+                print('a neprost', a)
+                b = 1
+            else:
+                pass
+    if b != 1:
+        print('a prost', a)
+        p = p + [a]
+    count += 1
+    a = a + 2
+print(p)
